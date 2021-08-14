e377a5c83eae191b7edc4fa07def143bdb72dd48
sreelakshmi12998 <sreelakshmi12998@gmail.com>
2021-08-14 11:41:38 +0530

    Added feature- mark as done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import TodoApp from "./Components/TodoApp/TodoApp";
+import TodoCheckbox from "./Components/TodoApp/TodoCheckBox";
 
 function App() {
   return (
     <div className="App">
       {/*<TodoApp />*/}
       <TodoApp />
+      {/* <TodoCheckbox />  */}
     </div>
   );
 }
