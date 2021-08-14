0fb96849215707f1b6f1ad31ebeb174f21454203
sreelakshmi12998 <sreelakshmi12998@gmail.com>
2021-08-14 12:17:47 +0530

    added feature- mark as done

--- src/Components/TodoApp/TodoApp.js
+++ src/Components/TodoApp/TodoApp.js
@@ -24,11 +24,7 @@ function TodoApp() {
     }
   };
   const deleteTodo = (todoToBeDeleted) => {
-<<<<<<< HEAD
     setItems(items.filter((data, index) => index !== todoToBeDeleted));
-=======
-    setItems(items.filter((data, index) => index !== todoToBedeleted));
->>>>>>> f82493d822a7127e43c84a4b09939a50fedcabbb
   };
 
   return (
@@ -46,7 +42,6 @@ function TodoApp() {
       <ul>
         {items.map((data, index) => (
           <li key={index}>
-<<<<<<< HEAD
             <div class="wrap">
               <div class="blocks">
                 <input
@@ -71,14 +66,6 @@ function TodoApp() {
                 </button>
               </div>
             </div>
-=======
-            <input type="checkbox" />{" "}
-            <span className="data-section">{data}</span>
-            <FaTrash
-              className="trash-icon"
-              onClick={() => deleteTodo(index)}
-            />
->>>>>>> f82493d822a7127e43c84a4b09939a50fedcabbb
           </li>
         ))}
       </ul>
